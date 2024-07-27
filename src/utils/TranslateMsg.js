80fb2b7c3616be09649d10e3b43a6c0af646d4f3
Mthunzi Tom <mthunzitom.10@gmail.com>
2024-07-27 20:05:27 +0200

    Added Google cloud translate api

---
+++ src/utils/TranslateMsg.js
@@ -0,0 +1,7 @@
+import TranslateMsgAPI from "./TranslateMsgAPI";
+
+export default function TranslateMsg(message, langValue) {
+    // Create a variable to store the translated messages
+    // Use the TranslateMsgAPI function to translate the messages and store them in the translatedMessages variable
+    TranslateMsgAPI(message, langValue);
+}
